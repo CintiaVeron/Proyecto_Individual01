3c02f76235b671ee43125554db1b3f1127eb6fba
CintiaVeron <veroncintia@gmail.com>
2024-03-29 20:27:52 -0300

    act

--- main.py
+++ main.py
@@ -16,83 +16,46 @@ async def root():
 
 
 # Cargar los datos si es necesario
-merged_df = pd.read_parquet("Archivos_API/PlayTimeGenre.parquet")
+df_UsersRecommend = pd.read_parquet("Archivos_API/UsersRecommend.parquet")
 
 
 
 # Definir tu aplicación FastAPI
 app = FastAPI()
 
-# Definir la función PlayTimeGenre con manejo de errores
-#PlayTimeGenre
-@app.get("/PlayTimeGenre/{genero :str}")
-async def PlayTimeGenre(genero):
+# Definir la función UserRecommend con manejo de errores
+@app.get("/UserRecommend/{año:int}")
+async def UserRecommend(año: int):
     try:
-        # Filtrar por género
-        merged_df_genero = merged_df[merged_df['genero'].str.contains(genero, case=False)]
+        # Filtrar las recomendaciones para el año dado y recomendaciones positivas/neutrales
+        recomendaciones = df_UsersRecommend[df_UsersRecommend['posted'] == año]
 
-        # Si no hay datos para el género, lanzar una excepción HTTP 404
-        if merged_df_genero.empty:
-            raise HTTPException(status_code=404, detail=f"No se encontraron datos para el género {genero}")
+        # Si no hay datos para el año, lanzar una excepción HTTP 404
+        if recomendaciones.empty:
+            raise HTTPException(status_code=404, detail=f"No hay recomendaciones para el año {año}")
 
-        # Calcular las horas jugadas por año de lanzamiento
-        horas_por_anio = merged_df_genero.groupby('anio_lanzamiento')['playtime_forever'].sum()
+        # Ordenar en orden descendente por la cantidad de recomendaciones
+        recomendaciones = recomendaciones.sort_values('recommend', ascending=False)
 
-        # Encontrar el año con más horas jugadas
-        año_max_horas = horas_por_anio.idxmax()
-
-        # Devolver el resultado
-        return {f"Año de lanzamiento con más horas jugadas para {genero}": año_max_horas}
-    
-    except HTTPException as e:
-        # Si ya es una excepción HTTP, solo relanzarla
-        raise e
-    except Exception as e:
-        # En caso de cualquier otro error, lanzar una excepción HTTP 500
-        raise HTTPException(status_code=500, detail=str(e))
-
-
-
-
-# Definir la función para UserForGenre 
-
-
-@app.get("/UserForGenre/{genero :str}")
-async def UserForGenre(genero):
-    try:
-        # Filtrar por género
-        merged_df_genero = merged_df[merged_df['genero'].str.contains(genero, case=False)]
-
-        # Si no hay datos para el género, lanzar una excepción HTTP 404
-        if merged_df_genero.empty:
-            raise HTTPException(status_code=404, detail=f"No se encontraron datos para el género {genero}")
-
-        # Encontrar el usuario que acumula más horas jugadas
-        user_max_horas = merged_df_genero.groupby('user_id')['playtime_forever'].sum().idxmax()
-    
-        # Calcular la acumulación de horas jugadas por año
-        horas_por_anio = merged_df_genero.groupby('anio_lanzamiento')['playtime_forever'].sum().reset_index()
-        horas_por_anio = horas_por_anio.rename(columns={'anio_lanzamiento': 'Año', 'playtime_forever': 'Horas'})
-        horas_por_anio = horas_por_anio.to_dict(orient='records')
+        # Crear una única línea de resultado con los tres primeros juegos recomendados
+        resultado = {
+            "Puesto 1": recomendaciones.iloc[0]['app_name'],
+            "Puesto 2": recomendaciones.iloc[1]['app_name'],
+            "Puesto 3": recomendaciones.iloc[2]['app_name']
+        }
 
         # Devolver el resultado
         return {
-        f"Usuario con más horas jugadas para {genero}": user_max_horas,
-        "Horas jugadas": horas_por_anio
-    }
-    
+            f"El top 3 de los juegos más recomendados para el año {año} son": resultado
+        }
+
     except HTTPException as e:
         # Si ya es una excepción HTTP, solo relanzarla
         raise e
     except Exception as e:
         # En caso de cualquier otro error, lanzar una excepción HTTP 500
         raise HTTPException(status_code=500, detail=str(e))
-
-
-
-
-
-
+    
 
 
 if __name__ == "__main__":
